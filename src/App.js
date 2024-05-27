c65814f2112c80c62f85ba3136923fa9710d792d
dayoung <leedayeonggg@gmail.com>
2024-05-27 11:35:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
+// 최근에 함수형으로 바뀌고 있는 추세
+/*
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
+}
+*/
+
+// class App extends Component{
+//   render(){
+//     return(
+//           <div className="App">
+//       Hello World
+//     </div>
+//     )
+//   }
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      Hello World
+    </div>
+    )
+  }
 }
 
 export default App;
